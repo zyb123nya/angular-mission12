3d2045e1d3b65576e37637569dd16749e295e130
zyb123nya <1846172115@qq.com>
2021-06-17 14:48:09 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-mission12
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-mission12)
---
+++ src/app/cart.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+  items: any[] = [];
+
+  addToCart(product: any) {
+    this.items.push(product);
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  clearCart() {
+    this.items = [];
+    return this.items;
+  }
+  constructor(private http: HttpClient) {}
+  getShippingPrices() {
+    return this.http.get<{ type: string; price: number }[]>(
+      '/assets/shipping.json'
+    );
+  }
+}
